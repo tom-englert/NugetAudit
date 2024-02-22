3f524599f980ab02970a6264fe5dec4ecaf8e742
tom-englert <thomas.englert@avl.com>
2024-02-22 09:49:17 +0100

    Initial Seed

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # NugetAudit
-A DotNet tool to create a vulnerability report from an applications *.deps.json file
+A DotNet command line tool to create a vulnerability report from an applications *.deps.json file
+
+## Intention of this tool
+This tool can be used to create a vulnerability report based on the binary deliveries, excluding all build time dependencies.
+
+Unlike `dotnet list package --vulnerable` it only works on the build output, not on the sources, 
+so it generates a reproducible result even when projects use e.g. floating package versions.
+
+It's e.g. useful to create a snapshot of the know vulnerabilities at release time and then compare with the current state on a periodical base,
+so new vulnerabilities that come up later can be detected and customers can be informed 
+to update the software if the vulnerability might affect the product.
+
+## Installation
+`dotnet tool install TomsToolbox.NugetAudit -g`
+
+## Usage
+`nuget-audit [file or directory]`
+
+### Arguments
+- **no arguments**: all *.deps.json in the current directory will be scanned
+- **file**: the specified file is scanned
+- **directory**: all *.deps.json in the specified directory will be scanned
+
+Sample output:
+```json
+{
+  "reportVersion": 1,
+  "packages": {
+    "Microsoft.IdentityModel.JsonWebTokens.6.32.2.0": {
+      "id": "Microsoft.IdentityModel.JsonWebTokens",
+      "version": "6.32.2.0",
+      "vulnerabilities": [
+        {
+          "severity": 1,
+          "advisoryUrl": "https://github.com/advisories/GHSA-8g9c-28fc-mcx2"
+        },
+        {
+          "severity": 1,
+          "advisoryUrl": "https://github.com/advisories/GHSA-59j7-ghrg-fj52"
+        }
+      ]
+    },
+    "System.IdentityModel.Tokens.Jwt.6.32.2.0": {
+      "id": "System.IdentityModel.Tokens.Jwt",
+      "version": "6.32.2.0",
+      "vulnerabilities": [
+        {
+          "severity": 1,
+          "advisoryUrl": "https://github.com/advisories/GHSA-8g9c-28fc-mcx2"
+        },
+        {
+          "severity": 1,
+          "advisoryUrl": "https://github.com/advisories/GHSA-59j7-ghrg-fj52"
+        }
+      ]
+    }
+  }
+}
+```
+
+## Nuget configuration
+
+To be able to retrieve vulnerabilities, a source repository that supports this endpoint is needed, see e.g. [Where do CVE/GHSA come from?](https://devblogs.microsoft.com/nuget/how-to-scan-nuget-packages-for-security-vulnerabilities/)
+
+This tool only uses the first end point provided in you nuget.config and has only been tested against `https://api.nuget.org/v3/index.json`; the NuGet configuration is read starting at the current working directory, see [](https://learn.microsoft.com/en-us/nuget/consume-packages/configuring-nuget-behavior#config-file-locations-and-uses)
+
+However, if there isn't some very customized configuration, this should be the default.
---
+++ src/NugetAudit/NugetContext.cs
@@ -0,0 +1,79 @@
+﻿using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Packaging.Core;
+using NuGet.Protocol.Core.Types;
+
+namespace DependencyModel;
+
+internal sealed class NugetContext : IDisposable
+{
+    private readonly TaskCompletionSource<NugetResources> _nugetResources = new ();
+
+    public SourceRepository SourceRepository { get; }
+
+    public SourceCacheContext CacheContext { get; } = new();
+
+    public NugetContext()
+    {
+        var settings = Settings.LoadDefaultSettings(Directory.GetCurrentDirectory());
+        var packageSourceProvider = new PackageSourceProvider(settings);
+        var sourceRepositoryProvider = new SourceRepositoryProvider(packageSourceProvider, Repository.Provider.GetCoreV3());
+        var sourceRepositories = sourceRepositoryProvider.GetRepositories();
+    
+        SourceRepository = sourceRepositories.First();
+
+        GetResourcesInternal(SourceRepository);
+    }
+
+    public Task<NugetResources> Resources => _nugetResources.Task;
+
+    public async Task<Package?> GetPackageInfo(PackageIdentity packageIdentity)
+    {
+        var resources = await Resources.ConfigureAwait(false);
+
+        var versions = await resources.FindPackageById.GetAllVersionsAsync(packageIdentity.Id, CacheContext, NullLogger.Instance, CancellationToken.None).ConfigureAwait(false);
+        if (versions?.Any() != true)
+            return null;
+
+        var metadata = await resources.PackageMetadata.GetMetadataAsync(packageIdentity, CacheContext, NullLogger.Instance, CancellationToken.None).ConfigureAwait(false);
+
+        if (metadata is not { Vulnerabilities: { } vulnerabilityMetadata })
+            return null;
+
+        var vulnerabilities = vulnerabilityMetadata.Select(v => new Vulnerability(v.Severity, v.AdvisoryUrl)).ToArray();
+
+        if (vulnerabilities.Length == 0)
+            return null;
+
+        var package = new Package(
+            packageIdentity.Id,
+            packageIdentity.Version.Version,
+            vulnerabilities);
+
+        return package;
+    }
+
+    private async void GetResourcesInternal(SourceRepository sourceRepository)
+    {
+        try
+        {
+            var packageResource = await sourceRepository
+                .GetResourceAsync<FindPackageByIdResource>(CancellationToken.None).ConfigureAwait(false);
+            var packageMetadataResource = await sourceRepository
+                .GetResourceAsync<PackageMetadataResource>(CancellationToken.None).ConfigureAwait(false);
+
+            _nugetResources.SetResult(new NugetResources(packageResource, packageMetadataResource));
+        }
+        catch (Exception ex)
+        {
+            _nugetResources.SetException(ex);
+        }
+    }
+
+    public void Dispose()
+    {
+        CacheContext.Dispose();
+    }
+}
+
+internal sealed record NugetResources(FindPackageByIdResource FindPackageById, PackageMetadataResource PackageMetadata);
